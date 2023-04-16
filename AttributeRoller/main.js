284dd050caf5a28d5a4906886ebda992b25c771d
htorres5 <99515061+htorres5@users.noreply.github.com>
2023-04-16 16:13:31 -0700

    committed most recent version lol

--- AttributeRoller/main.js
+++ AttributeRoller/main.js
@@ -1,3 +1,7 @@
+
+
+const defaultAttributeScores = [15, 14, 13, 12, 10, 8];
+
 class Player {
     constructor(characterName = 'Luffy') {
         this.name = characterName;
@@ -9,7 +13,13 @@ class Player {
             wisdom: 0,
             charisma: 0
         };
+        let shuffledResult = shuffleArray(defaultAttributeScores);
+        for (const [key, value] of Object.entries(this.attributes)) {
+            let attributeValue = shuffledResult.pop();
+            this.attributes[key] = attributeValue;
+        }
     }
+    
 
     rollAttributes () {
         for (const key in this.attributes) {
@@ -33,6 +43,17 @@ class Player {
             console.log(`${key.slice(0,3).toUpperCase()}: ${value}`)
         }
     }
+}
+
+function shuffleArray(targetArray) {
+    let shuffled = Array.from(targetArray);
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = shuffled[i];
+        shuffled[i] = shuffled[j];
+        shuffled[j] = temp;
+    }
+    return shuffled;
 }
 
 function diceRoller(times, sides) {
